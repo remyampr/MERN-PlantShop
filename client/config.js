e8a60af24539202e30c263e0ae7cffd75966c267
remyampr <remyaradhakrishnan2992@gmail.com>
2025-02-10 19:19:21 +0400

    backend url changed

--- client/config.js
+++ client/config.js
@@ -1,6 +1,8 @@
 // Configuration file for API base URL
 const config = {
-    API_BASE_URL: "http://localhost:5100", // Default for local development
+    // API_BASE_URL: "http://localhost:5100", 
+    API_BASE_URL: "https://mern-plant-shopserver-lvijpjn4z-remyas-projects-a6393d66.vercel.app/", 
+    /// Default for local development
   };
   
   export default config;
