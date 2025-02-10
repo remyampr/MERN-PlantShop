d85b33ce074d690c70e46a54f8641e40e6fa453e
remyampr <remyaradhakrishnan2992@gmail.com>
2025-02-10 20:26:21 +0400

    commit

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 // Configuration file for API base URL
 const config = {
     // API_BASE_URL: "http://localhost:5100", 
-    API_BASE_URL: "https://mern-plant-shopserver.vercel.app/", 
+    API_BASE_URL: "https://mern-plant-shopserver.vercel.app", 
     /// Default for local development
   };
   
